e5980b5816b692ba1010abdb77b05e7caa5dde8d
Satvik Manchanda <101499823+Satvik1769@users.noreply.github.com>
2023-10-05 11:11:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Signup from './pages/signUp';
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Login from './pages/login';
+
 
 const Stack = createStackNavigator()
 
@@ -10,7 +10,8 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Sign up" component={Signup} />
    
     </Stack.Navigator>
   </NavigationContainer>
